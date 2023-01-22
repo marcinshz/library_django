6029869a6e5af2581facd10195a8e8da3d99e62a
Damian Bugaj <81823937+Dejman636363@users.noreply.github.com>
2023-01-22 22:46:32 +0100

    dodanie endpointów

--- library/polls/models.py
+++ library/polls/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Book(models.Model):
-    name = models.CharField(max_length = 16)
-    author = models.CharField(max_length = 16)
+    name = models.CharField(max_length = 30)
+    author = models.CharField(max_length = 30)
     def __str__(self):
         return self.name
 
 class User(models.Model):
-    name = models.CharField(max_length = 16)
-    borrowed_books = models.ManyToManyField(Book)
+    name = models.CharField(max_length = 30)
+    borrowed_books = models.ManyToManyField(Book, null=True, blank=True)
     def __str__(self):
         return self.name
--- library/polls/views.py
+++ library/polls/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework import permissions
 from .models import Book, User
 from .serializers import BookSerializer, UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['post'])
+    def add_book(self, request, pk=None):
+        user = self.get_object()
+        book = Book.objects.get(pk=request.data['book_id'])
+        user.borrowed_books.add(book)
+        return Response({'status': 'book added'})
+
+    @action(detail=True, methods=['post'])
+    def remove_book(self, request, pk=None):
+        user = self.get_object()
+        book = Book.objects.get(pk=request.data['book_id'])
+        user.borrowed_books.remove(book)
+        return Response({'status': 'book removed'})
+    
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
